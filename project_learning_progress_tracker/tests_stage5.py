9536774792f7e075737f59d80990975691941b6e
nurgalive <azat.nur@gmail.com>
2024-02-28 09:03:02 +0100

    Added new files

---
+++ project_learning_progress_tracker/tests_stage5.py
@@ -0,0 +1,15 @@
+import unittest
+
+from stage_5 import (
+    Student,
+    Submission,
+)
+
+class TestNotifyWithoutData(unittest.TestCase):
+    pass
+
+class TestNotifyWithData(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
